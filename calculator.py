b1b7c860e2d3ea8f4e0ec30bf7ea430cb9d1adcd
Jenna Fong <jfong569@gmail.com>
2021-03-17 03:46:15 +0000

    added code to arithmetic.py and calculator.py

--- calculator.py
+++ calculator.py
@@ -1,7 +1,33 @@
 """CLI application for a prefix-notation calculator."""
 
+
+
 from arithmetic import (add, subtract, multiply, divide, square, cube,
-                        power, mod, )
+                        power, mod)
 
+con = True
 
-# Replace this with your code
+while con:
+    input_string = input("> ")
+    tokens = input_string.split(' ')
+
+    if tokens[0] == "q":
+        con = False
+    else:
+        if tokens[0] == "+":
+            print(add(int(tokens[1]),int(tokens[2])))
+        elif tokens[0] =="-":
+            print(subtract(int(tokens[1]),int(tokens[2])))
+        elif tokens[0] =="*":
+            print(multiply(int(tokens[1]),int(tokens[2])))
+        elif tokens[0] =="/":
+            print(divide(int(tokens[1]),int(tokens[2])))
+        elif tokens[0] =="square":
+            print(square(int(tokens[1])))
+        elif tokens[0] =="cube":
+            print(cube(int(tokens[1])))
+        elif tokens[0] =="pow":
+            print(power(int(tokens[1]),int(tokens[2])))
+        else:
+            tokens[0] =="mod"
+            print(mod(int(tokens[1]),int(tokens[2])))
